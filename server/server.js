810ef6ce4d117f06c5e20abb4ca5e75e878d484a
Ahmed Mustafa <ahmed@example.com>
2026-05-28 13:54:19 +0300

    dffg

--- server/server.js
+++ server/server.js
@@ -5,8 +5,8 @@ import { Server } from 'socket.io';
 import { connectedDb } from './config/db.js';
 import { UserRouter } from './routes/User.js';
 import { OwnerRouter } from './routes/owner.js';
-import {logsRouter } from './routes/Logs.js' 
-import {GateRouter } from './routes/gates.js' 
+import { logsRouter } from './routes/Logs.js'; 
+import { GateRouter } from './routes/gates.js'; 
 import cors from 'cors';
 import qrRouter from './routes/qrRouter.js';
 
@@ -15,16 +15,26 @@ const port = 4000;
 
 // الـ Middlewares
 app.use(express.json());
-app.use(cors());
+
+// 1. تشغيل الـ CORS بالروابط الكاملة مكتوبة نصوص صريحة جوة المصفوفة مباشرة
+app.use(cors({
+    origin: ["https://sedi-henesh-gate.surge.sh", "https://sedi-henesh-gate-admin.surge.sh"],
+    credentials: true
+}));
+
+app.options('*', cors({
+    origin: ["https://sedi-henesh-gate.surge.sh", "https://sedi-henesh-gate-admin.surge.sh"],
+    credentials: true
+}));
 
 // 2. تحويل الـ app إلى HTTP Server عشان السوكيت يشتغل صح
 const httpServer = createServer(app);
 
-// 3. إعداد الـ Socket.io وتمريره للـ global عشان نقدر نستخدمه في الكنترولر
+// 3. إعداد الـ Socket.io وتمرير نفس الروابط الكاملة له
 const io = new Server(httpServer, {
     cors: {
-        origin: ["http://localhost:5173","http://localhost:5174","http://localhost:5175"],
-        methods: ["GET", "POST"]
+        origin: ["https://sedi-henesh-gate.surge.sh", "https://sedi-henesh-gate-admin.surge.sh"],
+           credentials:true
     }
 });
 global.io = io; // الحركة دي بتخلينا ننده io.emit في أي مكان في المشروع
